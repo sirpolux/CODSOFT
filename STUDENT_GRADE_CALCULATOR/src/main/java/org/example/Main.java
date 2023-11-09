c4ff768c6a6f3746853302e1843e76d7c218a097
sirpolux <poluxreloaded@gmail.com>
2023-11-09 17:41:27 +0100

    final commit STUDENT GRADE CALCULATOR

--- STUDENT_GRADE_CALCULATOR/src/main/java/org/example/Main.java
+++ STUDENT_GRADE_CALCULATOR/src/main/java/org/example/Main.java
@@ -9,32 +9,33 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        printDeco(32,'*');
+        printDeco(50,'*');
         System.out.println("***        STUDENT GRADE CALCULATOR       ***");
-        printDeco(32,'*');
+        printDeco(50,'*');
         System.out.println();
         System.out.print("Hello, may i have you name:  ");
         String teacherName = scanner.nextLine();
         System.out.println();
-        System.out.print("Nice to have you "+teacherName);
+        System.out.print("Nice to have you "+teacherName +"\n");
         int totalSubjects=getUserInputNum(scanner,"Enter total subject offered by student");
         ArrayList<Subject> subjectList = new ArrayList<>();
         System.out.println("Total subject: "+totalSubjects);
 
         double total =0.0;
-        double average=0.0;
-        double percentage=0.0;
         int maxScore=100;
 
-
+        Scanner scanner1 = new Scanner(System.in);
+        printDeco(50,'_');
         for(int i =0; i<totalSubjects; i++){
-            System.out.println("Subjects captured");
+            System.out.println("Subjects captured:  "+ i);
             Subject subject = new Subject();
-            subject.setSubjectName(getUserInputText(scanner,"Subject:  "));
+            subject.setSubjectName(getUserInputText(scanner1,"Subject:  "));
             subjectList.add(subject);
         }
+        printDeco(50,'_');
+
         for (Subject subject:subjectList) {
-            double score = getUserInputDouble(scanner,subject.getSubjectName());
+            double score = getUserInputDouble(scanner1,subject.getSubjectName());
             subject.setScore(score);
             subject.setGrade(computeGrade(score));
             total+=score;
@@ -42,9 +43,17 @@ public class Main {
 
         //PRINT STUDENT PERFORMANCE
         double averagePercentage = ((double) total / (totalSubjects * maxScore)) * 100;
-        System.out.println("The average percentage of the scores is: " + averagePercentage + "%");
+        System.out.println();
+        printDeco(50,'_');
+        subjectList.forEach(Main::printStudentGrade);
+        printDeco(50,'_');
+        System.out.println("The total scores is: " + total);
+        System.out.printf("The average percentage of the scores is: %.2f",averagePercentage);
+        System.out.print("%");
 
-        average = total/totalSubjects;
+        //average = total/totalSubjects;
+        scanner1.close();
+        scanner.close();
 
     }
     static  int getUserInputNum(Scanner scanner, String text){
@@ -84,6 +93,6 @@ public class Main {
         return  "E";
     }
     static void printStudentGrade(Subject subject){
-        System.out.println();
+        System.out.println(subject.getSubjectName() + "  Score: " +subject.getScore()+ "  -> "+subject.getGrade());
     }
 }
